0c3ceec66b63d76af2ad94f6aba065832bfaff65
BobCashStory <47117399+BobCashStory@users.noreply.github.com>
2020-10-06 19:10:59 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function run(): Promise<void> {
     const dockerImage: string = core.getInput('docker_image')
     const defaultBranch: string = core.getInput('default_branch') || 'main'
     const tagWithSha: boolean = core.getInput('tag_with_sha') === 'true'
+    const customRef: string = core.getInput('ref')
 
-    const githubRef: string = process.env['GITHUB_REF'] || 'noop'
+    const githubRef: string = customRef || process.env['GITHUB_REF'] || 'noop'
     const githubSha: string = process.env['GITHUB_SHA'] || 'undefined'
     const githubEventName: string =
       process.env['GITHUB_EVENT_NAME'] || 'undefined'
